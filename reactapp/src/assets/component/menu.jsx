cad97f6f87b82f63c13a6187b7f94a29ecf8cfc4
Ishita Singh <isingh05439@gmail.com>
2026-04-08 13:24:55 +0530

    Initial commit of React project

---
+++ reactapp/src/assets/component/menu.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Menu(props){
+    return(
+    <>
+    <div>Menu</div>
+    <ul>
+        <li>Entertainment</li>
+        <li>Politics</li>
+        <li>Tech</li>
+        </ul>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {props.count}
+        </button>
+    </>
+    )
+}
+export default Menu
